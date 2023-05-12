9cbc875c840c165baf58e4017608f452c6e00dd6
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-12 16:41:14 -0400

    Check for factor

---
+++ check for factor.js
@@ -0,0 +1,5 @@
+const checkForFactor = (base, factor) => base % factor === 0;
+
+/*This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not. */
