eea9ef115116a8fff36692d31f867bc6cc5cc772
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-13 13:30:04 -0400

    double char - take 2

---
+++ 8kyu/Double Char - take 2.js
@@ -0,0 +1,9 @@
+const doubleChar = str => str.replace(/./g, '$&$&');
+
+/* Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Examples (Input -> Output):
+
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+*/
