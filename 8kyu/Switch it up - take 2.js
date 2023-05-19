d555db379439f7a56d1d205c902a885918a9e114
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-19 18:27:55 -0400

    Switch it up - take 2

---
+++ 8kyu/Switch it up - take 2.js
@@ -0,0 +1,9 @@
+const switchItUp = n => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][n]
+
+/*
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+*/
