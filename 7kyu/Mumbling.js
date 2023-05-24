224606353b08d8439c4cf2d1e5706481dad16190
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-23 21:35:01 -0400

    Mumblings - repeating characters

---
+++ 7kyu/Mumbling.js
@@ -0,0 +1,15 @@
+function accum(str) {
+	return str.split('')
+  .map((el,ind) => el.toUpperCase() + el.repeat(ind).toLowerCase())
+  .join('-')
+}
+
+/*This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
