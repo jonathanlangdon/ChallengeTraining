828ce9480478c2584fca7c98c8cd03d3411dd53d
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-27 22:47:08 -0400

    unfinished loop

---
+++ 8kyu/unfinished loop.js
@@ -0,0 +1,12 @@
+function createArray(number){
+  const arr = [];
+  for (let i = 1; i <= number; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+/* Unfinished Loop - Bug Fixing #1
+
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+*/
