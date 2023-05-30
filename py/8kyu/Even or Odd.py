5ffeba45b6fe90189d269e7dae1b57a929131b13
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-30 09:25:05 -0400

    updating comment

--- py/8kyu/Even or Odd.py
+++ py/8kyu/Even or Odd.py
@@ -1,4 +1,6 @@
 even_or_odd = lambda num: "Even" if num % 2 == 0 else "Odd"
 
-
-# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+'''
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+Another line
+'''
