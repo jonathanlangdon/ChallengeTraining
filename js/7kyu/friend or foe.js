db67a6ec1dc042028442c22dcb7afa7229a39256
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-01 23:23:15 -0400

    friend or foe comment update

--- js/7kyu/friend or foe.js
+++ js/7kyu/friend or foe.js
@@ -6,9 +6,5 @@ If a name has exactly 4 letters in it, you can be sure that it has to be a frien
 
 Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 
-i.e.
-
-friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
-
 Note: keep the original order of the names in the output.
 */
