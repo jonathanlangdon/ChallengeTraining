57a3b313f5733c5e8413a2662b7de22281ec8a74
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-01 23:21:22 -0400

    Two to One comment update

--- js/7kyu/Two to One.js
+++ js/7kyu/Two to One.js
@@ -1,4 +1,4 @@
-const longest = (s1, s2) => [...new Set((s1 + s2))].sort().join('');
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join('');
 
 /* Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 Examples:
@@ -6,9 +6,5 @@ Examples:
 a = "xyaabbbccccdefww"
 b = "xxxxyyyyabklmopq"
 longest(a, b) -> "abcdefklmopqwxy"
-
-a = "abcdefghijklmnopqrstuvwxyz"
-longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
-
 
 */
