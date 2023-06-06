d43c5b0e48536e243c9b6439f5d03e26fab29f94
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-05 22:26:58 -0400

    numbers divisble by given number

---
+++ py/8kyu/numbers divisble by given number.py
@@ -0,0 +1,9 @@
+def divisible_by(numbers, divisor):
+    return list(filter(lambda x: x % divisor == 0, numbers))
+
+/* divisor. First argument is an array of numbers and the second is the divisor.
+Example(Input1, Input2 --> Output)
+
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+*/
