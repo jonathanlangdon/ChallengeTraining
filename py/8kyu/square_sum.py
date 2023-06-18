432620089fce11f04efb30ccf3b2cd584956ecc2
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-18 15:14:00 -0400

    square sum using list comprehension

--- py/8kyu/square_sum.py
+++ py/8kyu/square_sum.py
@@ -1,3 +1,6 @@
+square_sum = lambda num: sum(n**2 for n in num)
+
+""" First Try
 def square_sum(numbers):
   sum = 0
   for el in numbers:
@@ -5,9 +8,11 @@ def square_sum(numbers):
   return sum
 
 print(square_sum([0, 3, 4, 5]))
+"""
 
-'''
+
+"""
 Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
-'''
+"""
