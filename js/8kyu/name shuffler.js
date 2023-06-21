02c9ef67c6c5f4c037afe3de2a003ff0bc9639f0
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-20 21:53:16 -0400

    name shuffler

---
+++ js/8kyu/name shuffler.js
@@ -0,0 +1,9 @@
+const nameShuffler = str => str.split(' ').reverse().join(' ')
+
+/* Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+*/
