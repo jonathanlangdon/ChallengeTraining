39dd83b3fad33d94b98921c91c9347dbfaef7e88
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-20 22:48:30 -0400

    name shuffler

---
+++ py/8kyu/name_shuffler.py
@@ -0,0 +1,10 @@
+name_shuffler = lambda str: ' '.join(str.split(' ')[::-1])
+
+'''
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+'''
