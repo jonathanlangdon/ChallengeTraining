26e86522d9c252f54c082e0d0eb0479d0263cdb5
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-23 15:10:28 -0400

    2nd way to do Boolean

--- js/8kyu/exclusive xor.js
+++ js/8kyu/exclusive xor.js
@@ -1,5 +1,7 @@
 const xor = (a, b) => Boolean(a ^ b)
 
+// note: could have done !!(a ^ b)
+
 /* In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
 
 false xor false == false // since both are false
