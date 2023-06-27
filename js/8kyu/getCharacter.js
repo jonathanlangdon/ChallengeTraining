307307970cb63ce9d2cd4de4c9eb0c133947c6cf
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-27 14:10:13 -0400

    // get character from ASCII Value

---
+++ js/8kyu/getCharacter.js
@@ -0,0 +1,14 @@
+// get character from ASCII Value
+
+const getChar = c => String.fromCharCode(c)
+
+/*Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+
+should return:
+
+'A'
+*/
