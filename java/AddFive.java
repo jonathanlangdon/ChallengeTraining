b904adf2f3d63d0f9972ce1f2ac9583ff6e0d1eb
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-01 07:28:01 -0400

    // Grasshopper - Basic Function Fixer

---
+++ java/AddFive.java
@@ -0,0 +1,25 @@
+// Grasshopper - Basic Function Fixer
+
+public class AddFive {
+  public static void main(String[] args) {
+      Kata kata = new Kata();
+      kata.printReturn();
+  }
+  
+  static class Kata {
+      public static int addFive(int num) {
+          return num + 5;
+      }
+      
+      public void printReturn() {
+          System.out.println(addFive(10));
+      }
+  }
+}
+
+/* Fix the function
+
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
