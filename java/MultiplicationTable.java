67b46cd600fad1da991d10ba9910c97d02e19339
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-01 07:28:16 -0400

    // Multiplication table for number

---
+++ java/MultiplicationTable.java
@@ -0,0 +1,42 @@
+// Multiplication table for number
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+      Kata kata = new Kata();
+      kata.printReturn();
+  }
+  
+  static class Kata {
+      public static String multiTable(int num) {
+        String result = "";
+        for(int i = 1; i <= 10; i++) {
+          result += String.format("%d * %d = %d\n", i, num, (i * num));
+        }
+        return result.trim();
+      }
+      
+      public void printReturn() {
+          System.out.println(multiTable(5));
+      }
+  }
+}
+
+/* Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+For example, a multiplication table (string) for number == 5 looks like below:
+
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+
+P. S. You can use \n in string to jump to the next line.
+
+Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+*/
