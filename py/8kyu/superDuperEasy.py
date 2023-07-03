d398a7172e6eee67ff6b922987596da0314c6ae3
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-03 07:17:13 -0400

    # Super Duper Easy

---
+++ py/8kyu/superDuperEasy.py
@@ -0,0 +1,5 @@
+# Super Duper Easy
+
+problem = lambda x: "Error" if type(x) == str else x * 50 + 6
+
+# Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".*/
