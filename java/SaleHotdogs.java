685404b55bf898dc7eb95fb1b658d51960b4d94e
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-04 18:35:10 -0400

    // Training JS #7: if..else and ternary operator

---
+++ java/SaleHotdogs.java
@@ -0,0 +1,23 @@
+// Training JS #7: if..else and ternary operator
+
+public class SaleHotdogs {
+    public static int saleHotdogs(final int n){
+        return n * (n < 5 ? 100 : n >= 5 && n < 10 ? 95 : 90);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SaleHotdogs.saleHotdogs(5));
+    }
+}
+
+
+/* Task:
+
+Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+number of hotdogs 	price per unit (cents)
+n < 5 	100
+n >= 5 and n < 10 	95
+n >= 10 	90
+
+You can use if..else or ternary operator to complete it.
+*/
