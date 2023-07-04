97d9004c70758218a8353a491164b824c6729985
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-04 18:35:19 -0400

    // The Wide-Mouthed frog!

---
+++ java/WideMouthedFrog.java
@@ -0,0 +1,21 @@
+// The Wide-Mouthed frog! 
+
+public class WideMouthedFrog {
+  static class Kata {
+    public static String mouthSize(String animal) {
+      return animal.toLowerCase().equals("alligator") ? "small" : "wide";
+    }
+  }
+  public static void main(String[] args) {
+    System.out.println(Kata.mouthSize("alLigator"));
+  }
+}
+
+/* The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+*/
