0818607c5b5b3b1179186851b38a1c761e49411a
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-05 07:27:13 -0400

    // Super Duper Easy

---
+++ js/8kyu/superDuperEasy.js
@@ -0,0 +1,5 @@
+// Super Duper Easy
+
+const problem = x => (typeof x === 'string' ? 'Error' : x * 50 + 6)
+
+/* Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".*/
