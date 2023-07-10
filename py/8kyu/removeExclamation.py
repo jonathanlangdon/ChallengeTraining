0ff55ffa9be5dbcf2f3e8b0597bfb8c3bcca551f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-09 22:24:25 -0400

    # Exclamation marks series #1: Remove an exclamation mark from the end of string

---
+++ py/8kyu/removeExclamation.py
@@ -0,0 +1,16 @@
+# Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+import re
+remove = lambda str: re.sub(r"!$", "", str)
+
+'''
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"
+'''
