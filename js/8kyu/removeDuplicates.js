d30aff0c7e9f0bbc61c9cb8d356f0cd8ff6117de
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-09 22:24:34 -0400

    // Remove duplicates from list

---
+++ js/8kyu/removeDuplicates.js
@@ -0,0 +1,9 @@
+// Remove duplicates from list
+
+const distinct = a => [...(new Set(a))]
+
+/*
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+*/
