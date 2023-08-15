face390c0b8ae05cacd242a2243bc111a63c71e8
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-15 13:19:43 -0400

    //Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0

---
+++ js/8kyu/terminalGameCombat.js
@@ -0,0 +1,6 @@
+//Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0
+
+function combat(health, damage) {
+  const newHealth = health - damage
+  return newHealth > 0 ? newHealth : 0
+}
