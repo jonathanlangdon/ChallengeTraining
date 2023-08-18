c7cceec00385fe122cda30d13cadb5d643978a4c
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-18 09:47:51 -0400

    //Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.

---
+++ js/8kyu/findTheRemainder.js
@@ -0,0 +1,11 @@
+//Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+function remainder(n, m) {
+  if (Math.min(n, m) === 0) {
+    return NaN
+  } else {
+    return Math.max(n, m) % Math.min(n, m)
+  }
+}
