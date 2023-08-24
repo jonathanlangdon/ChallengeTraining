277cc0b1218ebcf07199e2e1ab727229f3f53661
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-23 22:37:48 -0400

    // put items in array in a string joined by commas

---
+++ js/8kyu/withCommaDelimiters.js
@@ -0,0 +1,6 @@
+// put items in array in a string joined by commas
+
+const printArray = array => array.join(',')
+
+console.log(printArray(['h', 'o', 'l', 'a']))
+console.log(printArray([2, 4, 5, 2]))
