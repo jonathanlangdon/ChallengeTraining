8df4e329ed98b4a56b753715ccbe06142ea7a701
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-14 22:07:03 -0400

    // create function to detemrine derivative

---
+++ js/8kyu/derivative.js
@@ -0,0 +1,8 @@
+// create function to detemrine derivative
+
+function derive(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`
+}
+
+console.log(derive(7, 8)) //"56x^7"
+console.log(derive(5, 9)) // 45x^8
