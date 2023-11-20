affb8ceeac2e1efe5cf1b011ed8e9352e5ab0e40
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-19 22:49:35 -0500

    // given an integer or a floating-point number, find its opposite.

---
+++ c#/oppositeNumber.cs
@@ -0,0 +1,11 @@
+// given an integer or a floating-point number, find its opposite.
+
+using System;
+
+public class Kata
+    {
+        public  static int Opposite(int number)
+        {
+            return -number;
+        }
+    }
