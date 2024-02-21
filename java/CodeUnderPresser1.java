c67cce66da7bec6e674e49a0e13c55900a37c341
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-02-21 14:00:08 -0500

    class CodeUnderPressure1 {

---
+++ java/CodeUnderPresser1.java
@@ -0,0 +1,10 @@
+class CodeUnderPressure1 {
+  public static int doubleInteger(int i) {
+    return 2 * i;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(doubleInteger(3));
+  }
+}
