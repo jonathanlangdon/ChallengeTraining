21fd98b17ae3570c71f99da1a1d6118a424d95df
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-02-22 18:25:15 -0500

    public class ReturningStrings {

---
+++ java/ReturningStrings.java
@@ -0,0 +1,9 @@
+public class ReturningStrings {
+  public static String greet(String name) {
+    return "Hello, " + name + " how are you doing today?";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(greet("John"));
+  }
+}
