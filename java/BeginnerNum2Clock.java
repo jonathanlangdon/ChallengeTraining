a7ea41b8a84afbe8da2c2a1fb42f5c79f1c7474d
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-02-23 11:18:08 -0500

    public class BeginnerNum2Clock {

---
+++ java/BeginnerNum2Clock.java
@@ -0,0 +1,9 @@
+public class BeginnerNum2Clock {
+  public static int Past(int h, int m, int s) {
+    return (h * 3600 + m * 60 + s) * 1000;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Past(0, 1, 1)); // 61000
+  }
+}
