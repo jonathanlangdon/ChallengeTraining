8846d4bf4e99ffe93f233b38302c516b0b9f25c4
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-02-29 19:17:58 -0500

    class Upper{

---
+++ java/Upper.java
@@ -0,0 +1,8 @@
+class Upper{
+  public static String MakeUpperCase(String str){
+      return str.toUpperCase();
+  }
+    public static void main(String[] args) {
+		System.out.println(MakeUpperCase("ThisISit"));
+	}
+}
