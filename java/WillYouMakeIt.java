107720eb3f9fbf3517abc0ff849562c1f34075aa
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-03-12 07:37:27 -0400

    // determine if you have enough fuel to drive somewhere

---
+++ java/WillYouMakeIt.java
@@ -0,0 +1,26 @@
+// determine if you have enough fuel to drive somewhere
+
+public class WillYouMakeIt {
+
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+    return mpg * fuelLeft >= distanceToPump;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(zeroFuel(50, 25, 2)); // true
+    System.out.println(zeroFuel(100, 50, 1)); // false
+  }
+}
+
+/*
+ * You were camping with your friends far away from home, but when it's time to
+ * go back, you realize that your fuel is running out and the nearest pump is 50
+ * miles away! You know that on average, your car runs on about 25 miles per
+ * gallon. There are 2 gallons left.
+ * 
+ * Considering these factors, write a function that tells you if it is possible
+ * to get to the pump or not.
+ * 
+ * Function should return true if it is possible and false if not.
+ * 
+ */
