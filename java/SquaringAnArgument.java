7dd523f8544014401ff8c433c6558933dba51107
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-03-16 07:51:27 -0400

    // return the square of a number

---
+++ java/SquaringAnArgument.java
@@ -0,0 +1,12 @@
+// return the square of a number
+
+public class SquaringAnArgument {
+
+  public static int square(int n) {
+    return n * n;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(square(3)); // 9
+  }
+}
