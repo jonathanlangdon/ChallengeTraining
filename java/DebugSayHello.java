330200d4ad8342b538f83876ecf821b55e992f2f
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-03-17 07:38:30 -0400

    // fix a code that greets everyone by their name

---
+++ java/DebugSayHello.java
@@ -0,0 +1,12 @@
+// fix a code that greets everyone by their name
+
+public class DebugSayHello {
+
+  public static String sayHello(String name) {
+    return "Hello, " + name;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sayHello("Bob")); // Hello, Bob
+  }
+}
