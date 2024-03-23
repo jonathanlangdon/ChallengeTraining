dc2b4926a3e9f7e8f2db96f54ae0a638b2837cbd
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-03-23 07:42:10 -0400

    // sum up the goals Messie scored in 3 competitions

---
+++ java/MessiGoalTotal.java
@@ -0,0 +1,13 @@
+// sum up the goals Messie scored in 3 competitions
+
+public class MessiGoalTotal {
+  public static int laLigaGoals = 43;
+  public static int championsLeagueGoals = 10;
+  public static int copaDelReyGoals = 5;
+
+  public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+  public static void main(String[] args) {
+    System.out.println(totalGoals); // 58
+  }
+}
