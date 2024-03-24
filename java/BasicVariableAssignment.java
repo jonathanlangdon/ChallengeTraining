25442484718fb9de8099878ee2e6b1f646312e2c
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-03-24 07:46:52 -0400

    // fix basic variable assignment code

---
+++ java/BasicVariableAssignment.java
@@ -0,0 +1,12 @@
+// fix basic variable assignment code
+
+public class BasicVariableAssignment {
+
+  public static String a = "code";
+  public static String b = "wa.rs";
+  public static String name = a + b;
+
+  public static void main(String[] args) {
+    System.out.println(name); // codewa.rs
+  }
+}
