c0f05280e126d5bcf4abbaf20aebfcb7f92e3ee4
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-03 10:29:31 -0400

    // double integer and return integer

---
+++ c#/CantCodeUnderPressure1DoubleInteger.cs
@@ -0,0 +1,14 @@
+// double integer and return integer
+
+using System;
+
+public class CantCodeUnderPressure1DoubleInteger
+{
+
+	public static int DoubleInteger(int n) => n * 2;
+
+	public static void Main(string[] args)
+	{
+		Console.WriteLine(DoubleInteger(5)); //10
+	}
+}
