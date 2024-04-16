adb89d08cdfc3042e9b1952fe517235dc09feea6
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-16 19:46:00 -0400

    // convert a number type to string

---
+++ ts/convertNumberToString.ts
@@ -0,0 +1,13 @@
+// convert a number type to string
+
+export const numberToString = (num: number): string => num + '';
+
+console.log(typeof numberToString(3)); // string
+
+/* other methods:
+
+num.toString()
+String(num)
+$(num)
+
+*/
