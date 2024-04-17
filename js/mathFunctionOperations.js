23a28f1362b44c3798f4caf0d8f495cb64eb6564
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-17 15:12:20 -0400

    // create functions for math functions

---
+++ js/mathFunctionOperations.js
@@ -0,0 +1,8 @@
+// create functions for math functions
+
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+const subt = (a, b) => a - b;
