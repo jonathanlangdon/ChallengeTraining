cc48437a34509ac6d5b8c2f12f6916bbc17f8403
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-22 21:02:57 -0400

    // return the square of an argument

---
+++ c#/SquareAnArgument.cs
@@ -0,0 +1,11 @@
+// return the square of an argument
+
+public class SquareAnArgument
+{
+	public static double Square(double num) => num * num;
+
+	public static void Main(string[] args)
+	{
+		System.Console.WriteLine(Square(4.0)); // 16.0
+	}
+}
