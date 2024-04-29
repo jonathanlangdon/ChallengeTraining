592fd9021898f93a0a446cc179d0e1cca466aa3a
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-29 10:29:11 -0400

    commit file update

--- c#/0commitDailyAuto.sh
+++ c#/0commitDailyAuto.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Sleep for a random interval between 1 and 30 minutes
+sleep_time=$((RANDOM % 1740 + 60)) # This generates a random number between 60 and 1800
+echo "Sleeping for $sleep_time seconds."
+#sleep $sleep_time
+
 # Fetch the latest events for my github account
 events=$(curl -s https://api.github.com/users/jonathanlangdon/events)
 
@@ -8,8 +13,10 @@ today=$(date -u +"%Y-%m-%dT")
 
 # Check for any PushEvent today
 if echo "$events" | jq -e '.[] | select(.type == "PushEvent" and ( .created_at | tostring | startswith("'"$today"'"))) // empty' > /dev/null; then
-  echo "PushEvent found for today. Exiting script."
+  echo "push event found for today. Exiting script."
   exit 0
+else
+  echo "No push event found for today."
 fi
 
 # Generate a random number either 1 or 5
@@ -17,11 +24,6 @@ max_files=$(( ( RANDOM % 2 ) * 4 + 1 ))
 
 # Counter for the number of files processed
 count=0
-
-# Sleep for a random interval between 1 and 30 minutes
-sleep_time=$((RANDOM % 1740 + 60)) # This generates a random number between 60 and 1800
-echo "Sleeping for $sleep_time seconds."
-sleep $sleep_time
 
 # Check if there are any files in the 0staging folder
 if [ -n "$(ls -A ./0staging)" ]; then
