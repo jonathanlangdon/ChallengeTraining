acbee1562be75b134e5a167c9ec6766ba80ae6d7
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-05-18 08:58:49 -0400

    // create a method main() that prints hello world

---
+++ js/staticClassMainMethod.js
@@ -0,0 +1,10 @@
+// create a method main() that prints hello world
+
+// Print "Hello World!" to the screen
+class Solution {
+  static main(...args) {
+    console.log('Hello World!');
+  }
+}
+
+Solution.main('parameter1', 'parameter2');
