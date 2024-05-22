aee473e218380bddc34f18a7ae8bd337e6264269
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-05-22 18:06:49 -0400

    // convert boolean to string equiv

---
+++ ts/booleanToString.ts
@@ -0,0 +1,6 @@
+// convert boolean to string equiv
+
+export const booleanToString = (b: boolean): string => b.toString();
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
