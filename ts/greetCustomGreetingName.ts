17b5b76206e0965664bf34bbd159a5a4e9d3ebd8
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-05-22 18:06:52 -0400

    // return custom greeting

---
+++ ts/greetCustomGreetingName.ts
@@ -0,0 +1,7 @@
+// return custom greeting
+
+export function greet(name: string): string {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet('Fred')); // Hello, Fred how are you doing today?
