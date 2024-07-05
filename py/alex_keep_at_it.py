1ca6debd209b4d194babc213cc8724e744543d9e
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-07-05 18:23:54 -0400

    # encourage Alex in his hoola hooping

---
+++ py/alex_keep_at_it.py
@@ -0,0 +1,12 @@
+# encourage Alex in his hoola hooping
+
+
+def hoop_count(n):
+    if n >= 10:
+        return "Great, now move on to tricks"
+    else:
+        return "Keep at it until you get it"
+
+
+print(hoop_count(9))  # Keep at it
+print(hoop_count(10))  # Great...
