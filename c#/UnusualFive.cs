6a33a196368c5d63e00a0974b42b99f07ee68387
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-07-06 18:15:26 -0400

    // always return 5

---
+++ c#/UnusualFive.cs
@@ -0,0 +1,11 @@
+// always return 5
+
+public class AlwaysFive
+{
+	public static int UnusualFive() => "jesus".Length;
+
+	public static void Main(string[] args)
+	{
+		System.Console.WriteLine(UsualFive()); // 5
+	}
+}
