232da3e494a0d91e7053bf060cbea0a423c4cba7
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-07-09 19:16:08 -0400

    // create Ball object that accepts either one or no arguments

---
+++ c#/CreateBallObject.cs
@@ -0,0 +1,24 @@
+// create Ball object that accepts either one or no arguments
+
+public class CreateBallObject
+{
+	public class Ball {
+		public string ballType { get; set; }
+		
+		public Ball(string ballType) {
+			this.ballType = ballType;
+		}
+		
+		public Ball() {
+			this.ballType = "regular";
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		Ball ball1 = new Ball();
+		Ball ball2 = new Ball("super");
+		System.Console.WriteLine(ball1.ballType); // "regular"
+		System.Console.WriteLine(ball2.ballType); // "super"
+	}
+}
