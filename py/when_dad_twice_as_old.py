ede730560ff151541be75c6a5fdb749b7b4b8d82
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-07-17 18:27:02 -0400

    def twice_as_old(dad_years_old, son_years_old):

---
+++ py/when_dad_twice_as_old.py
@@ -0,0 +1,9 @@
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old / 2 - son_years_old) * 2
+
+
+print(twice_as_old(36, 7))  # 22
+print(twice_as_old(55, 30))  # 5
+print(twice_as_old(42, 21))  # 0
+print(twice_as_old(22, 1))  # 20
+print(twice_as_old(29, 0))  # 29
