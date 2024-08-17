d3793819a51b90dffdb1a0eef734e50e6668b23a
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-08-17 17:30:03 -0400

    // remove exclamation from end of string

---
+++ c#/RemoveExclamationLastCharacter.cs
@@ -0,0 +1,17 @@
+// remove exclamation from end of string
+
+using System.Linq;
+
+public class RemoveExclamationLastCharacter
+{
+	public static string Remove(string str)
+	{
+		return str.Last() == '!' ? str.Substring(0, str.Length - 1) : str;
+	}
+
+	public static void Main(string[] args)
+	{
+		System.Console.WriteLine(Remove("Th!is!")); // Th!is
+		System.Console.WriteLine(Remove("Th!at")); // Th!at
+	}
+}
