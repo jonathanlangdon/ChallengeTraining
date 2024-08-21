c8139928e40c642320fe12b38cb58131f88f14fd
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-08-20 22:38:29 -0400

    allowing support for decimals

--- ts/customEvalFunction.ts
+++ ts/customEvalFunction.ts
@@ -1,4 +1,4 @@
-// Adding support for negative numbers in Add/Subtract
+// Adding support for decimals
 
 export function calc(expression: string): number {
   const noSpaceExp: string = expression.replace(/\s/g, '');
@@ -35,7 +35,7 @@ function evaluateParenthesis(exp: string): string {
 
 function evaluateMultiplyDivide(exp: string): string {
   if (!/[*/]/.test(exp)) return exp;
-  let newExp = exp.replace(/-?\d+[*/]-?\d+/, multExp => {
+  let newExp = exp.replace(/-?\d+(\.\d+)?[*/]-?\d+(\.\d+)?/, multExp => {
     const negCountArr: string[] = multExp.match(/-/g) || [];
     const negCount: number = negCountArr ? negCountArr.length : 0;
     let isPositive = negCount === 1 ? false : true;
@@ -53,10 +53,10 @@ function evaluateMultiplyDivide(exp: string): string {
 }
 
 function evaluateAddSub(exp: string): string {
-  if (!/\d+[+-]/.test(exp)) return exp;
-  let newExp = exp.replace(/-?\d+[+-]-?\d+/, addSubExp => {
+  if (!/\d+(\.\d+)?[+-]/.test(exp)) return exp;
+  let newExp = exp.replace(/-?\d+(\.\d+)?[+-]-?\d+(\.\d+)?/, addSubExp => {
     addSubExp = addSubExp.replace('+-', '-').replace('--', '+');
-    const terms: string[] = [...(addSubExp.match(/[-+]*\d+/g) || [])];
+    const terms: string[] = [...(addSubExp.match(/[-+]*\d+(\.\d+)?/g) || [])];
     const result: number = Number(terms[0]) + Number(terms[1]);
     return String(result);
   });
@@ -69,24 +69,23 @@ function evaluateAddSub(exp: string): string {
 // console.log(evaluateMultiplyDivide("5*-6/2")); // -15
 // console.log(evaluateMultiplyDivide("5*6/-2")); // -15
 // console.log(evaluateMultiplyDivide("-5*-6/-2")); // -15
-console.log(evaluateAddSub('-3')); // -3
-console.log(evaluateAddSub('3')); // 3
-console.log(evaluateAddSub('-5-2')); // -7
-console.log(evaluateAddSub('5+6-2')); // 9
-console.log(evaluateAddSub('5+-6--2')); // 1
-console.log(evaluateAddSub('-5+6-2')); // -1
-console.log(evaluateAddSub('5+-6-2')); // -3
-console.log(evaluateAddSub('5+6--2')); // 13
-console.log(evaluateAddSub('-5+-6--2')); // -9
-console.log(evaluateAddSub('-5--6--2')); // 3
-console.log(evaluateAddSub('-5--6-1')); // 0
+// console.log(evaluateAddSub('-3')); // -3
+// console.log(evaluateAddSub('3')); // 3
+// console.log(evaluateAddSub('-5-2')); // -7
+// console.log(evaluateAddSub('5+6-2')); // 9
+// console.log(evaluateAddSub('5+-6--2')); // 1
+// console.log(evaluateAddSub('-5+6-2')); // -1
+// console.log(evaluateAddSub('5+-6-2')); // -3
+// console.log(evaluateAddSub('5+6--2')); // 13
+// console.log(evaluateAddSub('-5+-6--2')); // -9
+// console.log(evaluateAddSub('-5--6--2')); // 3
+// console.log(evaluateAddSub('-5--6-1')); // 0
 // console.log(evaluateAddSub("3+8-4")); // 7
 // console.log(evaluateParenthesis("5+(3)")); // 5+3
 // console.log(evaluateMultiplyDivide("5+2")); // 5+2
 // console.log(evaluateMultiplyDivide("5*6/2")); // 15
 // console.log(evaluateMultiplyDivide("3+5*6/2+2")); // 3+15+2
 // console.log(calc('10 + 20 * 3 / 2 - 5')); // 35
-// console.log(calc('2 / (2 + 3) * 4.33 - -6')); // 7.732
 // console.log(calc('1+1')); // 2
 // console.log(calc('1 - 1')); // 0
 // console.log(calc('1* 1')); // 1
@@ -96,5 +95,6 @@ console.log(evaluateAddSub('-5--6-1')); // 0
 // console.log(calc('1 /1')); // 1
 // console.log(calc('-123')); // -123
 // console.log(calc('123')); // 123
-// console.log(calc('2 /2+3 * 4.75- -6')); //21.25 ####
 // console.log(calc('12* 123')); // 1476
+console.log(calc('2 /2+3 * 4.75- -6')); //21.25
+console.log(calc('2 / (2 + 3) * 4.33 - -6')); // 7.732
