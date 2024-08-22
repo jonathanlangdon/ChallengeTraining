e319c9b8fe8bd6464de8594716b44e93093afa7d
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-08-22 19:00:02 -0400

    # First test added to check simple count

---
+++ py/validate_battlefield.py
@@ -0,0 +1,30 @@
+# First test added to check simple count
+
+
+def validate_battlefield(field):
+    if count_total_cells(field) != 20:
+        return False
+    else:
+        return True
+
+
+def count_total_cells(field):
+    flat_field = [cell for row in field for cell in row]
+    return flat_field.count(1)
+
+
+battleField = [
+    [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+print(count_total_cells(battleField))
+print(validate_battlefield(battleField))
