b57e3a1aaf2ab6ea167e6a32e1b2e6c5fda2c946
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-08-23 19:43:06 -0400

    put horizontal ships in a list

--- py/validate_battlefield.py
+++ py/validate_battlefield.py
@@ -1,4 +1,4 @@
-# check all rows for invalid layout
+# put horizontal ships in a list
 
 
 def validate_battlefield(field):
@@ -14,13 +14,32 @@ def count_total_cells(field):
 
 def has_no_diagonal_ships(field):
     # no ships should be diagonal with another
-    for j in range(0, 9):
-        for i in range(1, 10):
-            if field[j][i] == 1 and field[j + 1][i - 1] == 1:
+    for i in range(0, 9):
+        for j in range(1, 10):
+            if field[i][j] == 1 and field[i + 1][j - 1] == 1:
                 return False
-            if field[j][i - 1] == 1 and field[j + 1][i] == 1:
+            if field[i][j - 1] == 1 and field[i + 1][j] == 1:
                 return False
     return True
+
+
+def list_horiz_ships(field):
+    horiz_ship_list = []
+    for i in range(0, 10):
+        ship_size = 0
+        for j in range(0, 10):
+            cur_cell = field[i][j]
+            if cur_cell == 0 and ship_size > 0:
+                horiz_ship_list.append(ship_size)
+                ship_size = 0  # log ship size and continue
+            elif cur_cell == 1:
+                # check to make sure not vertical ship
+                if field[i - 1][j] == 1 or field[i + 1][j] == 1:
+                    continue
+                ship_size += 1  # update ship size
+            if j == 9 and ship_size > 0:
+                horiz_ship_list.append(ship_size)  # log last ship
+    return horiz_ship_list
 
 
 battleField = [
@@ -39,3 +58,4 @@ battleField = [
 print(count_total_cells(battleField))
 print(validate_battlefield(battleField))
 print(has_no_diagonal_ships(battleField))
+print(list_horiz_ships(battleField))
