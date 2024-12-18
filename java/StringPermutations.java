fe3f436b6b0fbc6e19473a28860c223a3d8ab801
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-12-18 09:38:56 -0500

    update 2: setting up hashSet

--- java/StringPermutations.java
+++ java/StringPermutations.java
@@ -1,13 +1,19 @@
-// give all permutations of string characters removing duplicates... Update 1: Test Case setup 
+// give all permutations of string characters removing duplicates... Update 2: setting up HashSet and for loops
 
 import java.util.*;
 
 public class StringPermutations {
     
-    public static List<String> singlePermutations(String s) {
+  public static List<String> singlePermutations(String s) {
+    Set<String> permSet = new HashSet<>();    
+    String[] characters = s.split("");
+    for (int i = 0; i < s.length; i++) {
+      for (int j = 0; j < s.length; j++) {
 
-        return new ArrayList<>(Arrays.asList(s.split("")));
+      }
     }
+    return new ArrayList<>(permSet);
+  }
 
 
   public static void main(String[] args){
