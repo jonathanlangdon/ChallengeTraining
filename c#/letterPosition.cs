579b804582eca7add3fa39068cb118b1fd83e075
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-10-21 13:58:34 -0400

    // output position in alphabet number

---
+++ c#/letterPosition.cs
@@ -0,0 +1,10 @@
+// output position in alphabet number
+
+public class Kata
+{
+  public static string Position(char alphabet)
+  {
+    int LetterPosition = (int)alphabet - 96;
+    return "Position of alphabet: " + LetterPosition;
+  }
+}
